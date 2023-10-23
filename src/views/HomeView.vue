7d4b29a7b0cab9f9c33676fee6b3a01a7a0db19e
AllinProgram <ufan0@foxmail.com>
2023-10-23 22:12:09 +0800

    修改提示语

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <main>
     <div class="chat chat-start">
-      <div class="chat-bubble">TA用网易云音乐在班群分享了一首歌，<br>好好听！好想知道TA的音乐账号！<br>这样就有更多话题了。</div>
+      <div class="chat-bubble">喜欢的TA在班群分享了一首歌，<br>好好听！好想知道TA的音乐账号！<br>这样就有更多话题了。</div>
     </div>
     <div class="chat chat-end">
       <div class="chat-bubble">
         这还不简单，复制TA发的音乐链接，<br>
         到<a href="https://findneteasecloudmusicuser.nclgclub.com/" target="_blank">这个网站</a>，
-        就能找得到TA的账号主页啦～
+        就能找得到TA的账号主页啦～<br>QQ音乐、网易云音乐都是支持的！
       </div>
     </div>
 
@@ -27,7 +27,7 @@
     <div class="chat chat-end">
       <div class="chat-bubble">
         快去试试呀，等你好消息！<br>
-        有问题的话可以直接在<label for="my-modal" class="btn btn-xs btn-success">作者公众号</label>寻求帮助喔。
+        有问题的话可以直接在<label for="my-modal" class="btn btn-xs btn-success">作者公众号</label>发消息寻求帮助喔。
         <input type="checkbox" id="my-modal" class="modal-toggle" />
         <div class="modal">
           <div class="modal-box">
