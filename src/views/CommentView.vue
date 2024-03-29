99e3da92cf9f1d95c35a6604e17727bc86d8e155
AllinProgram <ufan0@foxmail.com>
2024-03-29 15:20:34 +0800

    构建valine秘钥地址

--- src/views/CommentView.vue
+++ src/views/CommentView.vue
@@ -46,7 +46,9 @@ export default {
     mounted() {
         new Valine({
             el: '#vcomments',
-
+            // https://valine.js.org/
+            appId: '',
+            appKey: '',
             placeholder: "欢迎你来到这里，同时也欢迎你在这里畅所欲言，分享自己的故事~\n留言请尽量使用QQ邮箱（站内已经处理隐私，他人不可见），防止收不到回复。",
             meta: ['nick', 'mail', 'link'],
             recordIP: true,
